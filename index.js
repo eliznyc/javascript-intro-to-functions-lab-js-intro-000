13f4550885f4e501d5a48d462d755b321c0b408a
Elizabeth Peters <eliznyc@gmail.com>
2019-08-24 02:21:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
-  return "YES INDEED!"}
+  return 'YES INDEED!'}
 }
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-  return "I can't hear you!"}
+  return 'I can\'t hear you!'}
 }
 
 function sayHiToGrandma(string) {
